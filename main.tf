f3634b56d6d7ef2f36cb03fd4e8b555d22e84bfe
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-03-31 12:03:52 -0400

    rtdfgf

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 # Module definition
 
-provider "google" {
-  project = "avian-amulet-378416"
-  region  = "northamerica-northeast1"
-  zone = "northamerica-northeast1-a"
-}
-
-module "gce-lb-http" {
+/*
+  module "gce-lb-http" {
   source = "GoogleCloudPlatform/lb-http/google"
   version = "~> 4.4"
 
@@ -71,4 +66,55 @@ module "gce-lb-http" {
       }
     }
   }
+} 
+*/
+
+module "backend_service" {
+  source = "terraform-google-modules/serverless-backend/google"
+  name   = "backend-service"
+  type   = "cloud-run"
+
+  cloud_run_service_name = "my-cloud-run-service"
+  cloud_run_region       = "us-central1"
+}
+
+module "url_map" {
+  source         = "terraform-google-modules/http-load-balancer/google"
+  version        = "v4.0.0"
+  name           = "url-map"
+  default_service_backend = {
+    description           = "The backend service for the URL map"
+    backend_service       = module.backend_service.backend_service_id
+    timeout_sec           = 10
+  }
+
+  host_rules = [
+    {
+      hosts       = ["example.com"]
+      path_matcher = "path-matcher"
+    }
+  ]
+
+  path_matchers = [
+    {
+      name            = "path-matcher"
+      default_service = module.backend_service.backend_service_id
+      path_rules = [
+        {
+          paths   = ["/"]
+          service = module.backend_service.backend_service_id
+        }
+      ]
+    }
+  ]
+}
+
+module "global_forwarding_rule" {
+  source              = "terraform-google-modules/http-load-balancer/google"
+  version             = "v4.0.0"
+  name                = "global-forwarding-rule"
+  ip_address          = "global-appserver-ip"
+  port_range          = "80"
+  load_balancing_scheme = "EXTERNAL"
+  target              = module.url_map.target_self_link
 }
