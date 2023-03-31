8d905876ee1bfaabef18a1d3ab1bc41da338f753
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-03-30 22:05:11 -0400

    affasdfas

--- main.tf
+++ main.tf
@@ -3,13 +3,18 @@
 provider "google" {
   project = "avian-amulet-378416"
   region  = "northamerica-northeast1"
+  zone = "northamerica-northeast1-a"
 }
 
-module "load_balancer" {
-  source = "git::https://github.com/gruntwork-io/terraform-google-load-balancer.git?ref=master"
-  project = "avian-amulet-378416"
-  name = "my-http-load-balancer"
+module "http-load-balancer" {
+  source  = "terraform-google-modules/http-load-balancer/google"
+  version = "v1.4.0"
+  name               = "http-load-balancer"
   region  = "northamerica-northeast1"
   zone = "northamerica-northeast1-a"
-  enable_http = true
+  network            = "alex_net"
+  subnetwork         = "alex_subnet"
+  target_pool_ports  = ["80"]
+  target_pool_health_check = "tcp-health-check"
+  forwarding_rule_name = "http-forwarding-rule"
 }
