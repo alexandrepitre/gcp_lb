82e10426f73a7fb591cb97071953dabceefa4acb
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-03-30 21:54:23 -0400

    asfaf

--- main.tf
+++ main.tf
@@ -12,19 +12,4 @@ module "load_balancer" {
   region  = "northamerica-northeast1-a"
   zone = "northamerica-northeast1"
   enable_http = true
-
-  http_health_check {
-    request_path = "/"
-    port         = 80
-  }
-
-  backend_service {
-    name = "my-backend-service"
-
-    backend {
-      group = "my-instance-group"
-    }
-
-    timeout_sec = 10
-  }
 }
