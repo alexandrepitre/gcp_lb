18629e66e8a344407c813e4188a1024ec18dbacd
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-03-30 20:59:04 -0400

    fgdhgfhgh

--- main.tf
+++ main.tf
@@ -6,14 +6,59 @@ module "gce-lb-http" {
   region     = "northamerica-northeast1-a"
 
   enable_http = true #Enable plain http 
-  backend_service_port = 80
-  target_proxy_url_map = {
-    default_service = "http://my-backend-service.default.svc.cluster.local:80"
-    map             = null
-  }
+ backends = {
+    default = {
+      description                     = null
+      port                            = var.service_port
+      protocol                        = "HTTP"
+      port_name                       = var.service_port_name
+      timeout_sec                     = 10
+      enable_cdn                      = false
+      custom_request_headers          = null
+      custom_response_headers         = null
+      security_policy                 = null
 
-  global_forwarding_rule = {
-    ip_address = null
-    port_range = "80"
+      connection_draining_timeout_sec = null
+      session_affinity                = null
+      affinity_cookie_ttl_sec         = null
+
+      health_check = {
+        check_interval_sec  = null
+        timeout_sec         = null
+        healthy_threshold   = null
+        unhealthy_threshold = null
+        request_path        = "/"
+        port                = var.service_port
+        host                = null
+        logging             = null
+      }
+
+      log_config = {
+        enable = true
+        sample_rate = 1.0
+      }
+
+      groups = [
+        {
+          # Each node pool instance group should be added to the backend.
+          group                        = var.backend
+          balancing_mode               = null
+          capacity_scaler              = null
+          description                  = null
+          max_connections              = null
+          max_connections_per_instance = null
+          max_connections_per_endpoint = null
+          max_rate                     = null
+          max_rate_per_instance        = null
+          max_rate_per_endpoint        = null
+          max_utilization              = null
+        },
+      ]
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = null
+        oauth2_client_secret = null
+      }
+    }
   }
-}
