7117e348b8d963b05a5512f53f8dbb376c5ab8e0
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-03-30 22:01:53 -0400

    asffdfdsfd

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@
 
 provider "google" {
   project = "avian-amulet-378416"
-  region  = "northamerica-northeast1-a"
+  region  = "northamerica-northeast1"
 }
 
 module "load_balancer" {
   source = "git::https://github.com/gruntwork-io/terraform-google-load-balancer.git?ref=master"
   project = "avian-amulet-378416"
   name = "my-http-load-balancer"
-  region  = "northamerica-northeast1-a"
-  zone = "northamerica-northeast1"
+  region  = "northamerica-northeast1"
+  zone = "northamerica-northeast1-a"
   enable_http = true
 }
