b49784ff750eea40ca163071d0cf1ee768045d1b
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-04-18 19:54:25 -0400

    gfgddhfgfgd

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-#Reserved IP Address
-#resource "google_compute_global_address" "default" {
-#  name = "global-appserver-ip"
-#}
-
-# Use an existing Google Cloud Function
-#data "google_cloudfunctions_function" "my_function" {
-#  name = "function_v1_mtl"
-#}
-
 #Serverless Network Endpoint Group (NEG)
 resource "google_compute_region_network_endpoint_group" "function_neg" {
   name  = "function-neg"
@@ -19,59 +9,6 @@ resource "google_compute_region_network_endpoint_group" "function_neg" {
   }
 }
 
-#Create backend service
-#resource "google_compute_backend_service" "default" {
-#  name = "serverless-backend-service" 
-  #load_balancing_scheme = "EXTERNAL_MANAGED"
-  #protocol = "HTTP"
-  #timeout_sec = 10
-# Specify the Cloud Function as the backend
-#  backend {
-#    group  = data.google_cloudfunctions_function.my_function.name
-    #balancing_mode = "RATE"
-    #max_rate_per_instance = 10
-#  }
-#}
-
-/* # Create a URL map to route requests to the backend service
-resource "google_compute_url_map" "default" {
-  name            = "url-map-target-proxy"
-  default_service = google_compute_backend_service.default.id
-
-  host_rule {
-    hosts        = ["mysite.com"]
-    path_matcher = "allpaths"
-  }
-
-  path_matcher {
-    name            = "allpaths"
-    default_service = google_compute_backend_service.default.id
-
-    path_rule {
-      paths   = ["/*"]
-      service = google_compute_backend_service.default.id
-    }
-  }
-} */
-
-/* #Conifugre Target Proxy
-resource "google_compute_target_http_proxy" "default" {
-  name        = "target-proxy"
-  description = "a description"
-  url_map     = google_compute_url_map.default.id
-} */
-
-/* #Forwarding rule
-resource "google_compute_global_forwarding_rule" "default" {
-  name = "global-http-forwarding-rule"
-  project = "avian-amulet-378416"
-  target     = google_compute_target_http_proxy.default.id
-  ip_address = "global-appserver-ip"
-  port_range = "80"
-  load_balancing_scheme = "EXTERNAL"
-} */
-
-
 # Module definition
 module "lb-http-serverless" {
   source = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
@@ -82,17 +19,19 @@ module "lb-http-serverless" {
   create_address = true
   create_url_map = true
 
-  ssl                             = false
-  https_redirect                  = false
+  ssl                             = true
+  managed_ssl_certificate_domains = ["cn.com"]
+  https_redirect                  = true
   backends = {
     default = {
       description                     = null
-      protocol                        = "HTTP"
-      port_name                       = "HTTP"
+      protocol                        = "HTTPS"
+      port_name                       = "HTTPS"
       enable_cdn                      = false
       custom_request_headers          = null
       custom_response_headers         = null
       security_policy                 = null
+      compression_mode                = null
 
 
       log_config = {
